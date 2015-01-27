6349a12a68d1f6ca016e8217c5629af11b5b2556
Brian Woodward <brian.woodward@gmail.com>
2015-01-27 09:13:57 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * announcement <https://github.com/doowb/announcement>
+ *
+ * Copyright (c) 2015 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
